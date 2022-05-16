5abce4f86e8b31182c2c87259850c433d9705f30
M Hamza Saleem <mhamzasaleem3@gmail.com>
2022-05-16 14:49:33 +0500

    Two Strings Characters Matching

---
+++ TwoStringsCharactersMatching.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class TwoStringsCharactersMatching
+    {
+        static void Main(string[] args)
+        {
+            string name1 = "Aaad";
+            string name2 = "asad";
+            char[] nam1 = name1.ToCharArray();
+            char[] nam2 = name2.ToCharArray();
+            Array.Sort(nam1);
+            Array.Sort(nam2);
+            name1 = new String(nam1);
+            name2 = new String(nam2);
+            if (name1 == name2)
+                Console.WriteLine("Matched");
+            else
+                Console.WriteLine("Not Matched");
+
+            Console.ReadLine();
+        }
+    }
+}
